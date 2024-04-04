0405e16c97d055f62c27d3c13ef83898cc07b8fc
mhoyer <mathieuhoyer@gmail.com>
2024-04-04 17:14:04 +0200

    combak

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from '@/components/dark-mode/theme-provider';
 import './index.css';
 import { ModeToggle } from './components/dark-mode/mode-toggle';
 import { Game } from './components/board/Game';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const App = () => {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <ModeToggle />
             <Game />
-            <SpeedInsights />
         </ThemeProvider>
     );
 };
