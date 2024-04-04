bdd9f4653ec3a19601da45c35baff071c94fb284
mhoyer <mathieuhoyer@gmail.com>
2024-04-04 17:12:13 +0200

    majSpeed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from '@/components/dark-mode/theme-provider';
 import './index.css';
 import { ModeToggle } from './components/dark-mode/mode-toggle';
 import { Game } from './components/board/Game';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const App = () => {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <ModeToggle />
             <Game />
+            <SpeedInsights />
         </ThemeProvider>
     );
 };
